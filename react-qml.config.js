d63d8d646764915c0b9065044a02ce6e22ef90e8
Long Nguyen <long@podzim.co>
2019-03-03 20:23:49 +0700

    chore: fix travis build

--- react-qml.config.js
+++ react-qml.config.js
@@ -3,4 +3,9 @@ const path = require('path');
 module.exports = ({ root, platform }) => ({
   context: path.resolve(root, 'src'),
   entry: [`./index.qml`, `./index.js`],
+  output: {
+    path: path.join(root, 'native/dist'),
+    filename: `${platform}.bundle.js`,
+    library: 'Bundle',
+  },
 });
