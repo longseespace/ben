91345103634b0ae7e62e46e4a40e2bc1e17f00e6
Long Nguyen <long@podzim.co>
2019-04-17 21:59:57 +0700

    refactor: handle crashes in production only

--- src/index.tsx
+++ src/index.tsx
@@ -10,20 +10,20 @@ import configureStore from './store/configureStore';
 
 const { store, persistor } = configureStore();
 
-CrashHandler.init({
-  productName: 'Ben',
-  companyName: 'Podzim',
-  submitURL:
-    'https://sentry.io/api/1433538/minidump/?sentry_key=c621e085d91f467fa0d9629ba4375508',
-  uploadToServer: process.env.NODE_ENV !== 'production',
-  extra: {
-    sentry: {
-      release: pkg.version,
-      environment: process.env.NODE_ENV || 'development',
-      debug: process.env.NODE_ENV !== 'production',
+if (process.env.NODE_ENV === 'production') {
+  CrashHandler.init({
+    productName: 'Ben',
+    companyName: 'Podzim',
+    submitURL:
+      'https://sentry.io/api/1433538/minidump/?sentry_key=c621e085d91f467fa0d9629ba4375508',
+    uploadToServer: true,
+    extra: {
+      sentry: {
+        release: pkg.version,
+      },
     },
-  },
-});
+  });
+}
 
 export default (root: any) => {
   render(<App store={store} persistor={persistor} />, root);
