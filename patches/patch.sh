6148396a53eb5363684ff090a193a3f4aa76d24b
Long Nguyen <long@podzim.co>
2020-02-29 21:09:25 +0700

    refactor: patch Qt 5.14.1

--- patches/patch.sh
+++ patches/patch.sh
@@ -2,5 +2,5 @@ set -e
 
 echo 'Patching libqtlabsplatformplugin.dylib...'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cp -f $DIR/libqtlabsplatformplugin_dylib /usr/local/Cellar/qt/5\.13\.0/qml/Qt/labs/platform/libqtlabsplatformplugin\.dylib
+cp -f $DIR/libqtlabsplatformplugin_dylib /usr/local/Cellar/qt/5\.14\.1/qml/Qt/labs/platform/libqtlabsplatformplugin\.dylib
 echo 'Done'
